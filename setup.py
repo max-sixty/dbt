9e5956c323d2c627f2554164284ac4114cf165da
Maximilian Roos <m@maxroos.com>
2020-11-10 15:28:40 -0800

    18.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 package_name = "dbt"
 # package_version = "0.19.0b1"
 # Otherwise dbt-utils seems not to work.
-package_version = "0.18.3"
+package_version = "0.18.1"
 description = """With dbt, data analysts and engineers can build analytics \
 the way engineers build applications."""
 
